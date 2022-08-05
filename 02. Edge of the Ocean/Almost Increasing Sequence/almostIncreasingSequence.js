031e1e23ee6cb28fb6bb9640404e3724741065c5
zeusmarval <zeusmarval@gmail.com>
2022-08-04 23:43:58 -0400

    Update

--- 02. Edge of the Ocean/Almost Increasing Sequence/almostIncreasingSequence.js
+++ 02. Edge of the Ocean/Almost Increasing Sequence/almostIncreasingSequence.js
@@ -1,12 +1,12 @@
 var sequence = [1, 3, 2, 1];
 
 function solution(sequence) {
-    let invalidItemsCount = 0;
+    let count = 0;
   
     for (let i = 1; i < sequence.length; i++) {
       if (sequence[i] <= sequence[i-1]) {
-        invalidItemsCount++;
-        if (invalidItemsCount > 1) return false;
+        count++;
+        if (count > 1) return false;
         if (sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) return false;
       }
     }
